d629b13f33170ce24379a75e1f4c7534030b9f87
Eduardo Rasgado <eduardo.rasgado@hotmail.com>
2018-12-15 03:26:03 -0600

    Test executed using tags: Special tags

--- main.cpp
+++ main.cpp
@@ -145,3 +145,30 @@ TEST_CASE("Checking reconnection when connection goes off in all rooms in server
 {
     REQUIRE(2 == 2);
 }
+
+/*
+ * SPECIAL TAGS
+ *
+ *  [!throws] exclude if run catch with -e or --nothrow
+ *  example:
+ *      ./CppUnitTesting18 -e
+ *
+ *  exists more:
+ *      [!hide] or [.]
+ *      example: [.nameTest]
+ *
+ *      [!shouldfail] -> Reverse failing logic(pass if fail)
+ * */
+
+TEST_CASE("Checking the carrier module do not crash if full data is "
+          "called from redis(input is empty json and returns std::vector<mesasurement>)", "[!throws]")
+{
+    for (int i = 0; i < 100000000; ++i) {
+        // do nothing but simulates a slow test
+        for (int i = 0; i < 40; ++i) {
+
+        }
+    }
+    REQUIRE(1 ==1);
+}
+
