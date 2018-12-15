a400541e80f1d343cfbc763e0f2e0fd8bad3442f
Eduardo Rasgado <eduardo.rasgado@hotmail.com>
2018-12-14 22:56:58 -0600

    Importing library and defining first test.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN
+
+// found here:
+// https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp
+#include "catch.h"
+
+TEST_CASE("This is my first Test using Catch C++ Framework")
+{
+
+}
