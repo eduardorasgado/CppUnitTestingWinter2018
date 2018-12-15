da5aaa383dc358ddbe9779d01da8cae7d21f2f9b
Eduardo Rasgado <eduardo.rasgado@hotmail.com>
2018-12-15 02:07:56 -0600

    Command line testing.

--- main.cpp
+++ main.cpp
@@ -48,3 +48,41 @@ TEST_CASE("<Should> throw exception <When> invalid User") { REQUIRE(1 == 1); }
 TEST_CASE("<Given> authenticathed <When> invalid number used <Then> transaction will fail") { REQUIRE(1 == 1); }
 
 TEST_CASE("<Called> with empty list --> return nullptr") { REQUIRE(1 == 1); }
+
+
+/* COMMAND LINE TEST
+ *
+ * Run specific Test
+ *  thisTest
+ *  "This test"
+ *
+ * Run Group of test
+ *  Customer*
+ *  *Customer
+ *
+ * Run all tests except
+ *  Exclude: otherTest
+ *  ~otherTest
+ *  ~*other*
+ *
+ * Mixing test
+ *  a* ~ab* abc
+ *
+ * * TO MAKE THE TESTS: Just execute the bin or exe in console and using flags
+ *  Example:
+ *      ./CppUnitTesting18 --list-tests
+ *      ./CppUnitTesting18 --list-tests-names-only
+ *
+ *      ./CppUnitTesting18 "<Should>*"
+ *      ./CppUnitTesting18 "<Called>*" // all started like
+ *      ./CppUnitTesting18 *nullptr // all finishing in nullptr
+ *      ./CppUnitTesting18 *exception* //all with exception in between
+ * */
+
+TEST_CASE("<Should> throw exception <When> invalid Product") { REQUIRE(1 == 1); }
+TEST_CASE("<Called> with empty list --> return exception") { REQUIRE(1 == 1); }
+TEST_CASE("<Should> throw exception <When> invalid Alumno Element") { REQUIRE(1 == 1); }
+TEST_CASE("<Called> with empty list --> return false") { REQUIRE(1 == 1); }
+TEST_CASE("<Should> throw exception <When> nullptr") { REQUIRE(1 == 1); }
+TEST_CASE("<Called> with empty list --> return true") { REQUIRE(1 == 1); }
+TEST_CASE("<Should> throw exception <When> invalid Game mode") { REQUIRE(1 == 1); }
