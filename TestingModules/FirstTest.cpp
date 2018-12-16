af1b240f46c5129b433523aa55aabc5e3ec349d4
Eduardo Rasgado <eduardo.rasgado@hotmail.com>
2018-12-15 18:44:38 -0600

    Test: grouping tests in files

---
+++ TestingModules/FirstTest.cpp
@@ -0,0 +1,21 @@
+//
+// Created by cheetos on 15/12/18.
+//
+#include "../dependencies/catch.h"
+
+// First test: checking if framework is working
+TEST_CASE("This is my first Test using Catch C++ Framework")
+{
+// every unit enviroment should consist of three main parts
+
+// ARRANGE: Where we configure everything we need for the test
+std::string myString = "string";
+std::string mySecondString = "string";
+
+// ACT: Where actual test is done
+auto result = myString == mySecondString;
+
+// ASSERT: To check what the test did
+REQUIRE(result == true);
+
+}
--- main.cpp
+++ main.cpp
@@ -2,24 +2,7 @@
 
 // found here:
 // https://raw.githubusercontent.com/catchorg/Catch2/master/single_include/catch2/catch.hpp
-#include "catch.h"
-
-// First test: checking if framework is working
-TEST_CASE("This is my first Test using Catch C++ Framework")
-{
-    // every unit enviroment should consist of three main parts
-
-    // ARRANGE: Where we configure everything we need for the test
-    std::string myString = "string";
-    std::string mySecondString = "string";
-
-    // ACT: Where actual test is done
-    auto result = myString == mySecondString;
-
-    // ASSERT: To check what the test did
-    REQUIRE(result == true);
-
-}
+#include "dependencies/catch.h"
 
 /* TEST NAMES: should express a specific requirement/behavior
  * should include:
